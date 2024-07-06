9930e8972cac69cd0cdc31fe1172c78f3e6e209f
Abu Hanif <hanifcse90@gmail.com>
2024-07-06 23:00:25 +0600

    database connection with mongoose

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+import config from "./config";
+
+const port = 5000;
+async function main() {
+  try {
+    await mongoose.connect(config.databseURL as string);
+    app.listen(config.port, () => {
+      console.log(` App is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
